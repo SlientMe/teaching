4573ad3ae648ebda4d8d90c6d40942ece27ce1fb
Administrator <slient_me@outlook.com>
2019-09-02 15:45:07 +0800

    接球完成分数和生命

--- 郑继涵/中级/day08正式版.py
+++ 郑继涵/中级/day08正式版.py
@@ -46,26 +46,29 @@ while True:
 
     print_text(screen, font1, 0, 0, 'live:' + str(lives))
     print_text(screen, font1, 500, 0, 'socre:' + str(score))
+    print(lives)
     ballrect.y += speedy
     ballrect.x += speedx
-    print(ballrect.x)
 
 
     if ballrect.y > 700:  #   球超过屏幕
         ballrect.y = -30
         ballrect.x = random.randint(20, 480)
+        lives = lives - 1
 
     if ballrect.y < 0 and speedy<0:  #   球超过屏幕
         # speedx = -speedx
         speedy = -speedy
 
-    if ballrect.x<30or ballrect.x>610:   # 检查左右边缘
+    if ballrect.left<0 or ballrect.x>610:   # 检查左右边缘
         speedx = -speedx
 
     if ballrect.y > 650-30:              # 检查挡板是否借住
         if ballrect.x > sportrect.x and ballrect.x < sportrect.x + 200:
             speedx = -speedx
             speedy = -speedy
+            score = score+1
+
 
     keys = pygame.key.get_pressed()
 
