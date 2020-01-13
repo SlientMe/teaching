66a94ffef3f05a657894a0a40024f194d843a5b9
Administrator <slient_me@outlook.com>
2020-01-13 16:40:31 +0800

    笔记本

--- 郑继涵/高级/test.py
+++ 郑继涵/高级/test.py
@@ -1,37 +1,94 @@
-class Person(object):
-    """人"""
-    def __init__(self, name, age):
-        self._name = name
-        self._age = age
+from math import sqrt
+import sys
+from random import randint
 
-    def play(self):
-        print('%s正在愉快的玩耍.' % self._name)
-    def watch_av(self):
-        if self._age >= 18:
-            print('%s正在观看爱情动作片.' % self._name)
-        else:
-            print('%s只能观看《熊出没》.' % self._name)
+import pygame
 
-class Student():
-    """学生"""
-    def __init__(self, name, age, grade):
-        super().__init__(name, age)
-        self._grade = grade
-    def study(self, course):
-        print('%s的%s正在学习%s.' % (self._grade, self._name, course))
-    def play(self):
-        print('%s正在悲伤的玩耍.' % self.name)
+class Color():
+    """颜色"""
+
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    GRAY = (242, 242, 242)
+
+    @staticmethod
+    def random_color():
+        """获得随机颜色"""
+        r = randint(0, 255)
+        g = randint(0, 255)
+        b = randint(0, 255)
+        return (r, g, b)
 
 
-class Teacher(Student,Person):
-    """老师"""
-    def __init__(self, name, age, title):
-        super().__init__(name, age)
-        self._title = title
-    def title(self, title):
-        self._title = title
-    def teach(self, course):
-        print('%s%s正在讲%s.' % (self._name, self._title, course))
+class Ball(object):
+    """球"""
 
-llvbbo = Teacher('llvbbo',16,'good')
-llvbbo.play()
+    def __init__(self, x, y, radius, sx, sy, color=Color.RED):
+        """初始化方法"""
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.sx = sx
+        self.sy = sy
+        self.color = color
+        self.alive = True
+
+    def move(self, screen):
+        """移动"""
+        self.x += self.sx
+        self.y += self.sy
+        print(self.x)
+        if self.x - self.radius <= 0 or \
+                self.x + self.radius >= screen.get_width():
+            self.sx = -self.sx
+        if self.y - self.radius <= 0 or \
+                self.y + self.radius >= screen.get_height():
+            self.sy = -self.sy
+
+
+    def draw(self, screen):
+        """在窗口上绘制球"""
+        pygame.draw.circle(screen, self.color,
+                           (self.x, self.y), self.radius, 0)
+
+def main():
+    # 定义用来装所有球的容器
+    balls = []
+    # 初始化导入的pygame中的模块
+    pygame.init()
+    # 初始化用于显示的窗口并设置窗口尺寸
+    screen = pygame.display.set_mode((800, 600))
+    # 设置当前窗口的标题
+    pygame.display.set_caption('大球吃小球')
+    # 开启一个事件循环处理发生的事件
+    while True:
+        # 从消息队列中获取事件并对事件进行处理
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            # 处理鼠标事件的代码
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                # 获得点击鼠标的位置
+                x, y = event.pos
+                radius = randint(10, 100)
+                sx, sy = randint(-5, 5), randint(-5, 5)
+                color = Color.random_color()
+                # 在点击鼠标的位置创建一个球(大小、速度和颜色随机)
+                ball = Ball(x, y, radius, sx, sy, color)
+                # ball.draw(screen)
+        screen.fill((255, 255, 255))
+        try:
+            print(ball.sx)
+            ball.move(screen)
+            ball.draw(screen)
+
+        except:
+            print("mei you ball")
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    main()
--- 郑继涵/高级/打球吃小球.py
+++ 郑继涵/高级/打球吃小球.py
@@ -60,8 +60,10 @@ class Ball(object):
         """在窗口上绘制球"""
         pygame.draw.circle(screen, self.color,
                            (self.x, self.y), self.radius, 0)
-
+clock = pygame.time.Clock()  # 设置时钟
 def main():
+    clock.tick(60)  # 每秒执行60次
+
     # 定义用来装所有球的容器
     balls = []
     # 初始化导入的pygame中的模块
@@ -107,3 +109,100 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+'''
+from math import sqrt
+import sys
+from random import randint
+
+import pygame
+
+class Color():
+    """颜色"""
+
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    GRAY = (242, 242, 242)
+
+    @staticmethod
+    def random_color():
+        """获得随机颜色"""
+        r = randint(0, 255)
+        g = randint(0, 255)
+        b = randint(0, 255)
+        return (r, g, b)
+
+
+class Ball(object):
+    """球"""
+
+    def __init__(self, x, y, radius, sx, sy, color=Color.RED):
+        """初始化方法"""
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.sx = sx
+        self.sy = sy
+        self.color = color
+        self.alive = True
+
+    def move(self, screen):
+        """移动"""
+        self.x += self.sx
+        self.y += self.sy
+        print(self.x)
+        if self.x - self.radius <= 0 or \
+                self.x + self.radius >= screen.get_width():
+            self.sx = -self.sx
+        if self.y - self.radius <= 0 or \
+                self.y + self.radius >= screen.get_height():
+            self.sy = -self.sy
+
+
+    def draw(self, screen):
+        """在窗口上绘制球"""
+        pygame.draw.circle(screen, self.color,
+                           (self.x, self.y), self.radius, 0)
+
+def main():
+    # 定义用来装所有球的容器
+    balls = []
+    # 初始化导入的pygame中的模块
+    pygame.init()
+    # 初始化用于显示的窗口并设置窗口尺寸
+    screen = pygame.display.set_mode((800, 600))
+    # 设置当前窗口的标题
+    pygame.display.set_caption('大球吃小球')
+    # 开启一个事件循环处理发生的事件
+    while True:
+        # 从消息队列中获取事件并对事件进行处理
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            # 处理鼠标事件的代码
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                # 获得点击鼠标的位置
+                x, y = event.pos
+                radius = randint(10, 100)
+                sx, sy = randint(-5, 5), randint(-5, 5)
+                color = Color.random_color()
+                # 在点击鼠标的位置创建一个球(大小、速度和颜色随机)
+                ball = Ball(x, y, radius, sx, sy, color)
+                # ball.draw(screen)
+        screen.fill((255, 255, 255))
+        try:
+            print(ball.sx)
+            ball.move(screen)
+            ball.draw(screen)
+
+        except:
+            print("mei you ball")
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    main()
+'''
